ab3cf0b41792a45659df33852e2398d58ca4212d
«Svetlana» <timfr1209@gmail.com>
2022-10-26 15:55:54 +0300

    something interestingg

--- Homeworks/seminar7/Program.cs
+++ Homeworks/seminar7/Program.cs
@@ -39,7 +39,7 @@ Show2DArray(myArray);
 /*Например, задан массив: 1 4 7 2 
                           5 9 2 3
                           8 4 2 4       17 -> такого числа в массиве нет*/
-/*
+
 int[,] Create2DRandomArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, columns];                   
@@ -63,13 +63,13 @@ void Show2DArray (int[,] array)
     }   
 }
 
-bool UserElement (int[,] array, int num)
+bool UserElement (int[,] array, int a, int b)
 {
-     for(int i = 0; i < array.GetLength(0); i++)
+    for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {
-        if(array[i,j] == num)
+        if(array[i,j] == array[a,b])
         return true;
         }
     }
@@ -83,14 +83,16 @@ Console.WriteLine("Введите минимальное значение");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальное значение элемента");
 int max = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первый индекс");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второй индекс");
+int b = Convert.ToInt32(Console.ReadLine());
 
 int[,] myArray = Create2DRandomArray(m,n,min,max);
-bool result = UserElement (myArray, num);
+bool result = UserElement (myArray, a, b);
 Show2DArray(myArray);
 Console.WriteLine($"В данном массиве есть указанное вами число? -> {result}");
-*/
+
 // Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 /* Например, задан массив:
 1 4 7 2
